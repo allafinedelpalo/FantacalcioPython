f844d5e4843029f9cafc133d973d9dd025a493f6
alessio <alessio.antonielli@allafinedelpalo.it>
2016-11-28 20:50:17 +0100

    Aggiunto conteggio dei secondi di elaborazione

--- esporta_classifiche.py
+++ esporta_classifiche.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import itertools
 import openpyxl as xl
 from collections import Counter
@@ -15,19 +16,23 @@ def main(filename="Calendario.xlsx"):
 	utilizza queste informazioni per calcolare la classifica di tutti i fattoriale(Costanti.NUM_SQUADRE) calendari;
 	stampa la classifica dei calendari vinti dalle singole squadre, al variare dei punti in classifica"""
 	
+	starting_time = time.time()
 	squadre = []
 	giornate = [Giornata(n) for n in xrange(1, Costanti.NUM_GIORNATE+1)]
 	try:		
 		calendario_xl = xl.load_workbook(filename)
 	except Exception as e:
+		print 'Errore in lettura del file {}'.format(filename)
 		print e
 		print sys.exc_info()[0]
 	else:
+		print 'Letto il file {}'.format(filename)
 		calendario_sheet = calendario_xl.get_active_sheet()
 		squadre = utils.get_squadre_calendario(calendario_sheet)
 		utils.set_giornate_calendario(calendario_sheet, giornate, squadre)
 		all_permutations = list(itertools.permutations(squadre))
 		classifica_calendari = dict(zip(squadre, [0]*Costanti.NUM_SQUADRE))
+		print 'in elaborazione...'
 		for perm in all_permutations:
 			calendario = Calendario(perm, giornate)
 			calendario.calcola_classifica()
@@ -37,6 +42,7 @@ def main(filename="Calendario.xlsx"):
 				classifica_attuale[sc] = 1 
 			classifica_calendari = dict(Counter(classifica_calendari) + Counter(classifica_attuale))
 		utils.esporta_classifica_csv(classifica_calendari)
+		print 'Tempo impiegato: {0:.2f} s'.format(time.time() - starting_time)
 
  
 if __name__ == '__main__':
